0b24d75d4c4f5d6c3dddeb2038c69c3fcb06db76
fathel mohamud <fathel.mohamud@gmail.com>
2022-02-08 21:51:14 -0500

    fixed bugs and added tests

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,19 @@
+class SessionsController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    if user = User.authenticate_with_credentials(params[:email], params[:password])
+      session[:user_id] = user.id
+      redirect_to '/'
+    else
+      redirect_to '/login'
+    end
+  end
+
+  def destroy
+    session.delete(:user_id)
+    redirect_to '/login'
+  end
+end
